8d69962a0767c1d2bd6b34c60eec9227c678263f
Harrison Goldstein <hgoldstein95@gmail.com>
2016-11-16 21:30:15 -0500

    Stuff looking good.

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,45 +1,68 @@
 use std::collections::HashMap;
 use parser::{Aexp, Bexp, Com};
 
-pub struct Interpreter<'a> {
-    store: HashMap<&'a str, i32>,
+pub struct Interpreter {
+    store: HashMap<String, i32>,
 }
 
-impl <'a> Interpreter<'a> {
+impl Interpreter {
 
     pub fn new() -> Self {
         Interpreter { store: HashMap::new() }
     }
 
-    fn eval_aexp(&mut self, a: Aexp) -> i32 {
+    fn eval_aexp(&mut self, a: &Aexp) -> i32 {
         use parser::Aexp::*;
 
         match a {
-            Num(n) => n,
-            Var(s) => *self.store.get(s.as_str()).unwrap(), // TODO: handle err
-            Add(a1, a2) => self.eval_aexp(*a1) + self.eval_aexp(*a2),
-            Sub(a1, a2) => self.eval_aexp(*a1) - self.eval_aexp(*a2),
-            Mul(a1, a2) => self.eval_aexp(*a1) * self.eval_aexp(*a2),
+            &Num(n) => n,
+            &Var(ref s) => *self.store.get(s).unwrap(), // TODO: handle err
+            &Add(ref a1, ref a2) => self.eval_aexp(a1) + self.eval_aexp(a2),
+            &Sub(ref a1, ref a2) => self.eval_aexp(a1) - self.eval_aexp(a2),
+            &Mul(ref a1, ref a2) => self.eval_aexp(a1) * self.eval_aexp(a2),
         }
     }
 
-    fn eval_bexp(&mut self, b: Bexp) -> bool {
+    fn eval_bexp(&mut self, b: &Bexp) -> bool {
         use parser::Bexp::*;
 
         match b {
-            True => true,
-            False => false,
-            Eqs(a1, a2) => self.eval_aexp(*a1) == self.eval_aexp(*a2),
-            Less(a1, a2) => self.eval_aexp(*a1) < self.eval_aexp(*a2),
-            And(b1, b2) => self.eval_bexp(*b1) && self.eval_bexp(*b2),
-            Or(b1, b2) => self.eval_bexp(*b1) || self.eval_bexp(*b2),
+            &True => true,
+            &False => false,
+            &Eqs(ref a1, ref a2) => self.eval_aexp(a1) == self.eval_aexp(a2),
+            &Less(ref a1, ref a2) => self.eval_aexp(a1) < self.eval_aexp(a2),
+            &And(ref b1, ref b2) => self.eval_bexp(b1) && self.eval_bexp(b2),
+            &Or(ref b1, ref b2) => self.eval_bexp(b1) || self.eval_bexp(b2),
         }
     }
 
-    pub fn eval(&mut self, c: Com) -> HashMap<&'a str, i32> {
+    pub fn eval(&mut self, c: &Com) {
+        use parser::Com::*;
+
         match c {
-            _ => (),
-        };
+            &Skip => (),
+            &Seq(ref c1, ref c2) => { self.eval(c1); self.eval(c2) },
+            &Assgn(ref s, ref a) => {
+                let n = self.eval_aexp(a);
+                self.store.insert(s.clone(), n);
+            },
+            &If(ref b, ref c1, ref c2) => {
+                if self.eval_bexp(b) {
+                    self.eval(c1)
+                } else {
+                    self.eval(c2)
+                }
+            },
+            &While(ref b, ref c1) => {
+                if self.eval_bexp(b) {
+                    self.eval(c1);
+                    self.eval(c)
+                }
+            },
+        }
+    }
+
+    pub fn store(&self) -> HashMap<String, i32> {
         self.store.clone()
     }
 }
@@ -50,7 +73,7 @@ fn test_num() {
 
     let mut interp = Interpreter::new();
     let a = Num(42);
-    assert_eq!(42, interp.eval_aexp(a));
+    assert_eq!(42, interp.eval_aexp(&a));
 }
 
 #[test]
@@ -59,13 +82,33 @@ fn test_true() {
 
     let mut interp = Interpreter::new();
     let b = True;
-    assert!(interp.eval_bexp(b));
+    assert!(interp.eval_bexp(&b));
 }
 
 #[test]
-fn test_skip() {
-    use parser::Com::Skip;
+fn test_single_assgn() {
+    use parser::Com::Assgn;
+    use parser::Aexp::Num;
+
+    let prog = Assgn(String::from("x"), Box::new(Num(42)));
 
     let mut interp = Interpreter::new();
-    interp.eval(Skip);
+    interp.eval(&prog);
+    let s = interp.store();
+    assert_eq!(Some(&42), s.get(&String::from("x")));
+}
+
+#[test]
+fn test_double_assgn() {
+    use parser::Com::{Seq, Assgn};
+    use parser::Aexp::Num;
+
+    let prog = Seq(Box::new(Assgn(String::from("x"), Box::new(Num(42)))),
+                   Box::new(Assgn(String::from("y"), Box::new(Num(420)))));
+
+    let mut interp = Interpreter::new();
+    interp.eval(&prog);
+    let s = interp.store();
+    assert_eq!(Some(&42), s.get(&String::from("x")));
+    assert_eq!(Some(&420), s.get(&String::from("y")));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use std::io;
+
 mod lexer;
 mod parser;
 mod interpreter;
 
 use lexer::Lexer;
+use parser::Parser;
+use interpreter::Interpreter;
 
 fn main() {
-    let s = String::from("hello := 40; world := 2");
-    let mut lx = Lexer::new(s.chars());
-    lx.lex();
-    println!("Welcome to the 'impterpreter'!");
+    println!("Type IMP programs for interpretation.");
+    print!("> ");
+    let mut input = String::new();
+    try!(io::stdin().read_line(&mut input));
+
+    let l = Lexer::new(input.trim().chars());
+    let p = Parser::new(l.lex().into_iter());
+    let i = Interpreter::new();
+    println!("{:?}", i.eval(p.parse()));
 }
